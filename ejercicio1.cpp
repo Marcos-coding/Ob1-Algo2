581adbabb8d9b8a87f4e5db338197bec89d5bc43
Marcos-coding <marcosbellini045@gmail.com>
2026-09-02 15:52:00 -0300

    Creo clase AVL con nodos

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,6 +5,24 @@
 
 using namespace std;
 
+template <typename T> class AVL {
+    public: 
+        class Nodo {
+            public:
+                Nodo *izq, *der;
+                T valor;
+                
+                Nodo(T unValor){
+                  valor = unValor;
+                  izq = NULL; 
+                  der = NULL;
+                };
+        }
+
+
+};
+
+
 int main()
 {
     return 0;
